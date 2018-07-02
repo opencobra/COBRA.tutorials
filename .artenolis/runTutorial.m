4d6416158e04fab0723a8b71ad166f05c4aab000
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-02 17:45:29 +0200

    Error on purpose

--- .artenolis/runTutorial.m
+++ .artenolis/runTutorial.m
@@ -23,7 +23,8 @@ try
     initCobraToolbox(false)
 
     % set a default solver
-    changeCobraSolver('glpk'
+    changeCobraSolver('glpk');
+    error('test')
     fprintf('Default solver is set to GLPK\n');
 
     eval(tutorialName);
