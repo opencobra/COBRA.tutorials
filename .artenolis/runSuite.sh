5ba7b3cc0503f733482fa1716973d648b830b6f4
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-02 15:33:56 +0200

    Minor changes and comments in runSuite script

--- .artenolis/runSuite.sh
+++ .artenolis/runSuite.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# save the cloned directory
 tutorialsClonedPath=$(pwd)/../
 cd $tutorialsClonedPath
 mv linux tutorials
-mkdir linux
-cd linux
+mkdir linux && cd linux
+
+# clone the cobratoolbox
 git clone --depth=1 --no-single-branch https://github.com/opencobra/cobratoolbox.git cobratoolbox
 cd cobratoolbox
-git checkout ci-tutorials-new # checkout the branch on cobratoolbox (default: develop)
+
+# checkout the branch on cobratoolbox (default: develop)
+git checkout ci-tutorials-new
 
 # Remove the submodule entry from .git/config
 git submodule deinit -f tutorials
@@ -18,12 +22,16 @@ rm -rf .git/modules/tutorials
 # Remove the entry in .gitmodules and remove the submodule directory located at path/to/submodule
 git rm -f tutorials
 
+# commit the removed submodule
 git commit -m "Removed tutorials submodule"
 
+# initialize the submodules
 git submodule update --init --depth=1 --remote --no-fetch
 
-
-cd $tutorialsClonedPath # move out of the cobratoolbox directory
+# move the cloned tutorials folder to the cobratoolbox directory
+cd $tutorialsClonedPath
 mv tutorials linux/cobratoolbox/.
 cd linux/cobratoolbox
+
+# launch the script
 bash .artenolis/runTutorials.sh
