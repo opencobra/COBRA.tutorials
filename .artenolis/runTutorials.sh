4480b00ad213a251e9fb3b6b4139cc0e37cfa970
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-12 18:00:42 +0200

    adding browsenetwork

--- .artenolis/runTutorials.sh
+++ .artenolis/runTutorials.sh
@@ -65,7 +65,7 @@ buildTutorialList(){
 buildTutorialList
 
 # preliminary tutorials list
-declare -a tutorials=("tutorial_optForce", "tutorial_IO", "tutorial_COBRAconcepts")
+declare -a tutorials=("tutorial_optForce", "tutorial_IO", "tutorial_COBRAconcepts", "tutorial_browseNetwork")
 
 longest=0
 for word in "${tutorials[@]}"
