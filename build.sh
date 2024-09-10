aeb8fc563b03b6604d407446859c8cddc35e6bb1
AaronBrennan1 <68754265+AaronBrennan1@users.noreply.github.com>
2024-09-10 12:23:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,22 +5,26 @@ FILE_PATH=$2
 
 echo "Starting script execution..."
 
-# Convert mlx to html using MATLAB command
+# Convert mlx to html and pdf using MATLAB command
 # Note: Ensure that MATLAB is installed and added to the PATH
-echo "Converting .mlx file to .html..."
+echo "Converting .mlx file to .html and .pdf..."
 ABSOLUTE_FILE_PATH=$(realpath "$FILE_PATH")
 HTML_FILE_PATH=$(echo "$ABSOLUTE_FILE_PATH" | sed 's/.mlx/.html/g')
+PDF_FILE_PATH=$(echo "$ABSOLUTE_FILE_PATH" | sed 's/.mlx/.pdf/g')
 echo "Absolute file path: $ABSOLUTE_FILE_PATH"
 echo "HTML file path: $HTML_FILE_PATH"
+echo "PDF file path: $PDF_FILE_PATH"
 
 # Setup virtual frame buffer
 export DISPLAY=:100
 echo "Starting virtual frame buffer..."
 Xvfb -ac :100 -screen 0 1280x1024x24 > /dev/null &
 
-# Run MATLAB command to convert .mlx to .html
+# Run MATLAB command to convert .mlx to .html and .pdf
 echo "Running MATLAB conversion command..."
 /usr/local/MATLAB/R2024a/bin/matlab -batch "matlab.internal.liveeditor.openAndConvert('$ABSOLUTE_FILE_PATH', '$HTML_FILE_PATH')"
+echo "Converting to PDF..."
+/usr/local/MATLAB/R2024a/bin/matlab -batch "matlab.internal.liveeditor.openAndConvert('$ABSOLUTE_FILE_PATH', '$PDF_FILE_PATH')"
 
 # Clone the destination repository
 echo "Cloning the destination repository: https://github.com/$DEST_REPO.git"
@@ -50,9 +54,10 @@ TARGET_DIR="stable/tutorials/$(dirname "$FILE_PATH")"
 echo "Creating the target directory: $TARGET_DIR"
 mkdir -p "$TARGET_DIR"
 
-# Copy the HTML and .mlx files to the target directory in the destination repository
-echo "Copying the HTML and .mlx files to the target directory..."
+# Copy the HTML, PDF, and .mlx files to the target directory in the destination repository
+echo "Copying the HTML, PDF, and .mlx files to the target directory..."
 cp "$HTML_FILE_PATH" "$TARGET_DIR/"
+cp "$PDF_FILE_PATH" "$TARGET_DIR/"
 cp "$ABSOLUTE_FILE_PATH" "$TARGET_DIR/"
 
 # Add, commit, and push the changes
