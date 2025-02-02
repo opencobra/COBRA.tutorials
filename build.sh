ff39e1e5e76ad09a8ef39f0541adda648358cb84
pavan <138394293+pavan-kumar-s@users.noreply.github.com>
2025-02-02 12:05:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,26 +5,28 @@ FILE_PATH=$2
 
 echo "Starting script execution..."
 
-# Convert mlx to html and pdf using MATLAB command
+# Convert mlx to html, pdf and m using MATLAB command
 # Note: Ensure that MATLAB is installed and added to the PATH
-echo "Converting .mlx file to .html and .pdf..."
+echo "Converting .mlx file to .HTML, .pdf and .m..."
 ABSOLUTE_FILE_PATH=$(realpath "$FILE_PATH")
 HTML_FILE_PATH=$(echo "$ABSOLUTE_FILE_PATH" | sed 's/.mlx/.html/g')
 PDF_FILE_PATH=$(echo "$ABSOLUTE_FILE_PATH" | sed 's/.mlx/.pdf/g')
+M_FILE_PATH=$(echo "$ABSOLUTE_FILE_PATH" | sed 's/.mlx/.m/g')
 echo "Absolute file path: $ABSOLUTE_FILE_PATH"
 echo "HTML file path: $HTML_FILE_PATH"
 echo "PDF file path: $PDF_FILE_PATH"
+echo "PDF file path: $M_FILE_PATH"
 
 # Setup virtual frame buffer
 export DISPLAY=:100
 echo "Starting virtual frame buffer..."
 Xvfb -ac :100 -screen 0 1280x1024x24 > /dev/null &
 
-# Run MATLAB command to convert .mlx to .html and .pdf
+# Run MATLAB command to convert  .mlx file to .HTML, .pdf and .m..."
 echo "Running MATLAB conversion command..."
 /usr/local/MATLAB/R2024a/bin/matlab -batch "matlab.internal.liveeditor.openAndConvert('$ABSOLUTE_FILE_PATH', '$HTML_FILE_PATH')"
-echo "Converting to PDF..."
 /usr/local/MATLAB/R2024a/bin/matlab -batch "matlab.internal.liveeditor.openAndConvert('$ABSOLUTE_FILE_PATH', '$PDF_FILE_PATH')"
+/usr/local/MATLAB/R2024a/bin/matlab -batch "matlab.internal.liveeditor.openAndConvert('$ABSOLUTE_FILE_PATH', '$M_FILE_PATH')"
 
 # Clone the destination repository
 rm -rf cobratoolbox
@@ -52,11 +54,12 @@ TARGET_DIR="docs/tutorials/$(dirname "$FILE_PATH")"
 echo "Creating the target directory: $TARGET_DIR"
 mkdir -p "$TARGET_DIR"
 
-# Copy the HTML, PDF, and .mlx files to the target directory in the destination repository
-echo "Copying the HTML, PDF, and .mlx files to the target directory..."
+# Copy the HTML, PDF, mlx and .m files to the target directory in the destination repository
+echo "Copying the HTML, PDF, mlx and .m files to the target directory..."
 cp "$HTML_FILE_PATH" "$TARGET_DIR/"
 cp "$PDF_FILE_PATH" "$TARGET_DIR/"
 cp "$ABSOLUTE_FILE_PATH" "$TARGET_DIR/"
+cp "$M_FILE_PATH" "$TARGET_DIR/"
 
 # Add, commit, and push the changes
 echo "Adding changes to git..."
